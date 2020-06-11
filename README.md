a6651baa068f9dc980720791edc65129c0706743
Cyrus Yip <cyruscmyip1@gmail.com>
2020-06-11 09:50:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Unclickable button(?)
+
+> Makes an unclickable button by moving when hovered, requiring javascript, redirecting when devtools is open, setting the tabindex to -1 (un-tabable), not working with touch screens, and preventDefault-ing key presses (like enter key).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/0b181a4a-5b63-41c0-afef-403343abbc35/deploy-status)](https://app.netlify.com/sites/unclickable/deploys)
+
+Hosted on netlify: <https://unclickable.netlify.app>
---
+++ script.js
@@ -0,0 +1,51 @@
+PRODUCTION = false; // set to false if editing to use dev tools
+
+$(function () {
+  if ("ontouchstart" in document.documentElement) {
+    $("body").prepend(
+      "<h1 class='warning'>No touch screen devices allowed.</h1><p class='warning-text'>Or you could just press it. >:(</p>"
+    );
+  } else {
+    /* Stop context menu */
+    $(document).contextmenu(function (e) {
+      e.preventDefault();
+    });
+    /* Redirect dev tools */
+    $(window).on("devtoolschange", function () {
+      if (PRODUCTION) {
+        $("html").empty();
+        window.location.replace("/no.html");
+      }
+    });
+    /* Add button */
+    $("body").prepend("<button tabindex='-1'>Click me?</button>");
+    /* Move button */
+    $("button").mouseover(function () {
+      $(this).css("transform", "none");
+      $(this).animate(
+        {
+          left: Math.random() * ($(window).width() - $(this).width()) + "px",
+          top: Math.random() * ($(window).height() - $(this).height()) + "px",
+        },
+        50
+      );
+      // $(this).css("font-size", parseFloat($(this).css("font-size")) + 1); // Grow it!
+    });
+    /* Reset button */
+    $(window).resize(function () {
+      $("button").css({
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      });
+    });
+    /* Stop enter key and such */
+    $(document).on("keypress", function (e) {
+      e.preventDefault();
+    });
+    /* Button clicked */
+    $("button").click(function () {
+      alert("ive been clicked");
+    });
+  }
+});
